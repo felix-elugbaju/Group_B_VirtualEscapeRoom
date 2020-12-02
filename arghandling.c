fb1295b8e30abba3d5f5607042ad8a9d893ee012
felix-elugbaju <72533656+felix-elugbaju@users.noreply.github.com>
2020-12-01 19:59:38 -0500

    Update arghandling.c

--- arghandling.c
+++ arghandling.c
@@ -2,6 +2,10 @@
 #include <string.h>
 #include "arghandling.h"
 
+//Comment
+bool object_has_tag(OBJECT_t *obj, const char *arg){
+	return (arg != NULL) && (*arg != '\0') && (strcmp(arg, obj->tag) == 0);
+}
 OBJECT_t *get_object(const char *arg){
 	OBJECT_t *obj, *result = NULL;
 	for (obj = objs; obj < end_of_objs; obj++){			// scan through the object list (objs)

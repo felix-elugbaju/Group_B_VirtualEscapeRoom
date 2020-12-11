3e2d3b1e0ca9d9790a0a1712222cf8a5e7bbd116
Isaac Wiebe <Isaac.wiebe@me.com>
2020-12-11 17:10:17 -0500

    Removed all unix references from puzzle2 in puzzle.c

--- src/puzzle.c
+++ src/puzzle.c
@@ -58,272 +58,283 @@ void trigger_puzzle1(){
 
 void trigger_puzzle2()
 {
+    printf("Do you want to attempt sliding the pieces around? (y/n)"); // TODO: ADD
+    char user_intent = fgetc(stdin); // TODO: ADD
+    fflush(stdin);        // flush the input buffer
+    if(tolower(user_intent) == 'n'){ // TODO: ADD
+        return; //TODO: ADD
+    } // TODO: ADD
+    else //TODO: ADD
+    { //TODO: ADD
+    
 
-	char puzzle[PUZZLE_LENGTH][PUZZLE_LENGTH] = {
-		{'\\', '0', '/'},
-		{'.', '|', '.'},
-		{'/', '.', '\\'}
-		};
-	char puzzletwo[PUZZLE_LENGTH][PUZZLE_LENGTH];
-	for (int i = 0; i < PUZZLE_LENGTH; i++)
-	{
-		for (int j = 0; j < PUZZLE_LENGTH; j++)
-		{
-			if (puzzle[i][j] == '0')
-				puzzletwo[i][j] = '?';
-			else
-				puzzletwo[i][j] = puzzle[i][j];
-		}
-	}
+        char puzzle[PUZZLE_LENGTH][PUZZLE_LENGTH] = {
+            {'\\', '0', '/'},
+            {'.', '|', '.'},
+            {'/', '.', '\\'}
+            };
+        char puzzletwo[PUZZLE_LENGTH][PUZZLE_LENGTH];
+        for (int i = 0; i < PUZZLE_LENGTH; i++)
+        {
+            for (int j = 0; j < PUZZLE_LENGTH; j++)
+            {
+                if (puzzle[i][j] == '0')
+                    puzzletwo[i][j] = '?';
+                else
+                    puzzletwo[i][j] = puzzle[i][j];
+            }
+        }
 
-	printf("\n");
+        printf("\n");
 
-	printf("Use the WASD keys to rearrange the puzzle!\n");
-	printf("W => up\n");
-	printf("A => left\n");
-	printf("S => down\n");
-	printf("D => right\n");
-	printf("Enter 'q' to quit, or reset the puzzle\n");
-	printf("Type in enter to confirm your selection\n");
-	printf("The '?' is a free space to slide tiles in\n");
-	printf("\n");
-
-
-	int cursX = 1;
-	int cursY = 0;
-	for (int k = 0; k < 1000; k++)
-	{
-		int choice = rand() % 4;
-
-		switch (choice)
-		{
-			case 0:
-				if (cursY != PUZZLE_LENGTH - 1)
-				{
-					char temp = puzzletwo[cursY + 1][cursX];
-					puzzletwo[cursY + 1][cursX] = puzzletwo[cursY][cursX];
-					puzzletwo[cursY][cursX] = temp;
-					cursY++;
-
-				}
-				break;
-			case 1:
-				if (cursX != PUZZLE_LENGTH - 1)
-				{
-					char temp = puzzletwo[cursY][cursX + 1];
-					puzzletwo[cursY][cursX + 1] = puzzletwo[cursY][cursX];
-					puzzletwo[cursY][cursX] = temp;
-					cursX++;
-
-				}
-				break;
-			case 2:
-				if (cursY != 0)
-				{
-					char temp = puzzletwo[cursY - 1][cursX];
-					puzzletwo[cursY - 1][cursX] = puzzletwo[cursY][cursX];
-					puzzletwo[cursY][cursX] = temp;
-					cursY--;
-
-				}
-				break;
-			case 3:
-				if (cursX != 0)
-				{
-					char temp = puzzletwo[cursY][cursX - 1];
-					puzzletwo[cursY][cursX - 1] = puzzletwo[cursY][cursX];
-					puzzletwo[cursY][cursX] = temp;
-					cursX--;
-
-				}
-				break;
-			default:
-				break;
+        printf("Use the WASD keys to rearrange the puzzle!\n");
+        printf("W => up\n");
+        printf("A => left\n");
+        printf("S => down\n");
+        printf("D => right\n");
+        printf("Enter 'q' to quit, or reset the puzzle\n");
+        printf("Type in enter to confirm your selection\n");
+        printf("The '?' is a free space to slide tiles in\n");
+        printf("\n");
 
 
-		}
+        int cursX = 1;
+        int cursY = 0;
+        for (int k = 0; k < 100; k++)
+        {
+            int choice = rand() % 4;
+
+            switch (choice)
+            {
+                case 0:
+                    if (cursY != PUZZLE_LENGTH - 1)
+                    {
+                        char temp = puzzletwo[cursY + 1][cursX];
+                        puzzletwo[cursY + 1][cursX] = puzzletwo[cursY][cursX];
+                        puzzletwo[cursY][cursX] = temp;
+                        cursY++;
+
+                    }
+                    break;
+                case 1:
+                    if (cursX != PUZZLE_LENGTH - 1)
+                    {
+                        char temp = puzzletwo[cursY][cursX + 1];
+                        puzzletwo[cursY][cursX + 1] = puzzletwo[cursY][cursX];
+                        puzzletwo[cursY][cursX] = temp;
+                        cursX++;
+
+                    }
+                    break;
+                case 2:
+                    if (cursY != 0)
+                    {
+                        char temp = puzzletwo[cursY - 1][cursX];
+                        puzzletwo[cursY - 1][cursX] = puzzletwo[cursY][cursX];
+                        puzzletwo[cursY][cursX] = temp;
+                        cursY--;
+
+                    }
+                    break;
+                case 3:
+                    if (cursX != 0)
+                    {
+                        char temp = puzzletwo[cursY][cursX - 1];
+                        puzzletwo[cursY][cursX - 1] = puzzletwo[cursY][cursX];
+                        puzzletwo[cursY][cursX] = temp;
+                        cursX--;
+
+                    }
+                    break;
+                default:
+                    break;
 
 
-	}
-
-	printf("TARGET\n");
-	printf("\n");
+            }
 
 
-	for (int i = 0; i < PUZZLE_LENGTH; i++)
-	{
-		for (int j = 0; j < PUZZLE_LENGTH; j++)
-		{
-			printf("%c", puzzle[i][j]);
+        }
 
-		}
-		printf("\n");
-	}
-
-	printf("\n");
-	printf("\n");
-	printf("YOUR PUZZLE\n");
-	for (int i = 0; i < PUZZLE_LENGTH; i++)
-	{
-		for (int j = 0; j < PUZZLE_LENGTH; j++)
-		{
-			printf("%c", puzzletwo[i][j]);
-
-		}
-		printf("\n");
-	}
+        printf("TARGET\n");
+        printf("\n");
 
 
-	int done = 0;
+        for (int i = 0; i < PUZZLE_LENGTH; i++)
+        {
+            for (int j = 0; j < PUZZLE_LENGTH; j++)
+            {
+                printf("%c", puzzle[i][j]);
+
+            }
+            printf("\n");
+        }
+
+        printf("\n");
+        printf("\n");
+        printf("YOUR PUZZLE\n");
+        for (int i = 0; i < PUZZLE_LENGTH; i++)
+        {
+            for (int j = 0; j < PUZZLE_LENGTH; j++)
+            {
+                printf("%c", puzzletwo[i][j]);
+
+            }
+            printf("\n");
+        }
 
 
-	while (!done)
-	{
-		int flag1 = 1;
-
-		char buf[1];
-		read(1, &buf, sizeof(buf));
-		int input_char = buf[0];
-
-		if (input_char == 119)
-		{
-			if (cursY != PUZZLE_LENGTH - 1)
-			{
-				char temp = puzzletwo[cursY + 1][cursX];
-				puzzletwo[cursY + 1][cursX] = puzzletwo[cursY][cursX];
-				puzzletwo[cursY][cursX] = temp;
-				cursY++;
-
-			}
-
-		}
-		if (input_char == 97)
-		{
-			if (cursX != PUZZLE_LENGTH - 1)
-			{
-				char temp = puzzletwo[cursY][cursX + 1];
-				puzzletwo[cursY][cursX + 1] = puzzletwo[cursY][cursX];
-				puzzletwo[cursY][cursX] = temp;
-				cursX++;
-
-			}
-		}
-
-		if (input_char == 115)
-		{
-			if (cursY != 0)
-			{
-				char temp = puzzletwo[cursY - 1][cursX];
-				puzzletwo[cursY - 1][cursX] = puzzletwo[cursY][cursX];
-				puzzletwo[cursY][cursX] = temp;
-				cursY--;
-
-			}
-		}
-
-		if(input_char == 100)
-		{
-			if (cursX != 0)
-			{
-				char temp = puzzletwo[cursY][cursX - 1];
-				puzzletwo[cursY][cursX - 1] = puzzletwo[cursY][cursX];
-				puzzletwo[cursY][cursX] = temp;
-				cursX--;
-
-			}
-		}
-
-		if (input_char == 113)
-		{
-			printf("Resetting the puzzle\n");
-			return;
-		}
-
-		printf("\n");
-		printf("\n");
-
-		if (input_char != 10)
-		{
-			printf("Use the WASD keys to rearrange the puzzle!\n");
-			printf("W => up\n");
-			printf("A => left\n");
-			printf("S => down\n");
-			printf("D => right\n");
-			printf("Enter 'q' to quit, or reset the puzzle\n");
-			printf("Type in enter to confirm your selection\n");
-			printf("The '?' is a free space\n");
-			printf("\n");
-
-			printf("TARGET\n");
-			printf("\n");
-			for (int i2 = 0; i2 < PUZZLE_LENGTH; i2++)
-			{
-				for (int j2 = 0; j2 < PUZZLE_LENGTH; j2++)
-				{
-				printf("%c", puzzle[i2][j2]);
-				}
-				printf("\n");
-			}
-			printf("\n");
-			printf("\n");
-			printf("YOUR PUZZLE\n");
-			for (int i2 = 0; i2 < PUZZLE_LENGTH; i2++)
-			{
-				for (int j2 = 0; j2 < PUZZLE_LENGTH; j2++)
-				{
-					printf("%c", puzzletwo[i2][j2]);
-				}
-				printf("\n");
-			}
-		}
+        int done = 0;
 
 
+        while (!done)
+        {
+            int flag1 = 1;
+
+            int input_char;
+            input_char = fgetc(stdin);
+            
+
+            if (input_char == 119)
+            {
+                if (cursY != PUZZLE_LENGTH - 1)
+                {
+                    char temp = puzzletwo[cursY + 1][cursX];
+                    puzzletwo[cursY + 1][cursX] = puzzletwo[cursY][cursX];
+                    puzzletwo[cursY][cursX] = temp;
+                    cursY++;
+
+                }
+
+            }
+            if (input_char == 97)
+            {
+                if (cursX != PUZZLE_LENGTH - 1)
+                {
+                    char temp = puzzletwo[cursY][cursX + 1];
+                    puzzletwo[cursY][cursX + 1] = puzzletwo[cursY][cursX];
+                    puzzletwo[cursY][cursX] = temp;
+                    cursX++;
+
+                }
+            }
+
+            if (input_char == 115)
+            {
+                if (cursY != 0)
+                {
+                    char temp = puzzletwo[cursY - 1][cursX];
+                    puzzletwo[cursY - 1][cursX] = puzzletwo[cursY][cursX];
+                    puzzletwo[cursY][cursX] = temp;
+                    cursY--;
+
+                }
+            }
+
+            if(input_char == 100)
+            {
+                if (cursX != 0)
+                {
+                    char temp = puzzletwo[cursY][cursX - 1];
+                    puzzletwo[cursY][cursX - 1] = puzzletwo[cursY][cursX];
+                    puzzletwo[cursY][cursX] = temp;
+                    cursX--;
+
+                }
+            }
+
+            if (input_char == 113)
+            {
+                printf("Resetting the puzzle\n");
+                return;
+            }
+            
+
+            printf("\n");
+            printf("\n");
+
+            if (input_char != 10)
+            {
+                printf("Use the WASD keys to rearrange the puzzle!\n");
+                printf("W => up\n");
+                printf("A => left\n");
+                printf("S => down\n");
+                printf("D => right\n");
+                printf("Enter 'q' to quit, or reset the puzzle\n");
+                printf("Type in enter to confirm your selection\n");
+                printf("The '?' is a free space\n");
+                printf("\n");
+
+                printf("TARGET\n");
+                printf("\n");
+                for (int i2 = 0; i2 < PUZZLE_LENGTH; i2++)
+                {
+                    for (int j2 = 0; j2 < PUZZLE_LENGTH; j2++)
+                    {
+                    printf("%c", puzzle[i2][j2]);
+                    }
+                    printf("\n");
+                }
+                printf("\n");
+                printf("\n");
+                printf("YOUR PUZZLE\n");
+                for (int i2 = 0; i2 < PUZZLE_LENGTH; i2++)
+                {
+                    for (int j2 = 0; j2 < PUZZLE_LENGTH; j2++)
+                    {
+                        printf("%c", puzzletwo[i2][j2]);
+                    }
+                    printf("\n");
+                }
+            }
 
 
-		for (int i = 0; i < PUZZLE_LENGTH; i++)
-		{
-			for (int j = 0; j < PUZZLE_LENGTH; j++)
-			{
-				if (puzzletwo[i][j] != puzzle[i][j])
-				{
-					if (!(puzzletwo[i][j] == '?' && puzzle[i][j] == '0'))
-					{
-						flag1 = 0;
-					}
-				}
-			}
 
-		}
-		if (flag1)
-			done = 1;
-	}
-	printf("TARGET\n");
-	printf("\n");
-	for (int i2 = 0; i2 < PUZZLE_LENGTH; i2++)
-	{
-		for (int j2 = 0; j2 < PUZZLE_LENGTH; j2++)
-		{
-			printf("%c", puzzle[i2][j2]);
-		}
-		printf("\n");
-	}
-	printf("\n");
-	printf("\n");
-	printf("YOUR PUZZLE\n");
-	for (int i2 = 0; i2 < PUZZLE_LENGTH; i2++)
-	{
-		for (int j2 = 0; j2 < PUZZLE_LENGTH; j2++)
-		{
-			if (puzzletwo[i2][j2] == '?')
-					puzzletwo[i2][j2] = '0';
-			printf("%c", puzzletwo[i2][j2]);
-		}
-		printf("\n");
-	}
-	printf("The puzzle has been solved!\n");
-	sliding_puzzle->state = solved;
-    check_solved_stage1();
-	return;
+
+            for (int i = 0; i < PUZZLE_LENGTH; i++)
+            {
+                for (int j = 0; j < PUZZLE_LENGTH; j++)
+                {
+                    if (puzzletwo[i][j] != puzzle[i][j])
+                    {
+                        if (!(puzzletwo[i][j] == '?' && puzzle[i][j] == '0'))
+                        {
+                            flag1 = 0;
+                        }
+                    }
+                }
+
+            }
+            if (flag1)
+                done = 1;
+        }
+        printf("TARGET\n");
+        printf("\n");
+        for (int i2 = 0; i2 < PUZZLE_LENGTH; i2++)
+        {
+            for (int j2 = 0; j2 < PUZZLE_LENGTH; j2++)
+            {
+                printf("%c", puzzle[i2][j2]);
+            }
+            printf("\n");
+        }
+        printf("\n");
+        printf("\n");
+        printf("YOUR PUZZLE\n");
+        for (int i2 = 0; i2 < PUZZLE_LENGTH; i2++)
+        {
+            for (int j2 = 0; j2 < PUZZLE_LENGTH; j2++)
+            {
+                if (puzzletwo[i2][j2] == '?')
+                        puzzletwo[i2][j2] = '0';
+                printf("%c", puzzletwo[i2][j2]);
+            }
+            printf("\n");
+        }
+        printf("The puzzle has been solved!\n");
+        sliding_puzzle->state = solved;
+        check_solved_stage1();
+        return;
+    }
 }
 
 
